fb609cf51c9ce83da645c8b277d90325e702e0f5
Knimga <47860438+Knimga@users.noreply.github.com>
2019-06-03 19:27:02 -0600

    Update index.js
    Changed port# to 80, to work w/ Heroku.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.port || 3000;
+const port = process.env.port || 80;
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
