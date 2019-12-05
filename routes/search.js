17bc498a068157efca18e8c0a335b3132e700990
Evan <egrulke113@gmail.com>
2019-12-05 16:25:50 -0700

    added nav buttons between main page and equipment search

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 
 const Equipment = require('../models/equipment.model');
 
+routes.route('/').get((req,res) => {
+    res.status(200).send
+});
+
 routes.route('/quickSearch/:searchString').get((req,res) => {
     let searchString = req.params.searchString;
     Equipment.find({"itemName": { $regex: searchString, $options: 'gim'}})
